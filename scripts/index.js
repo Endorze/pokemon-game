9ade0ba8aa6090e8f411e5e70fddd0b96f793b52
Endorze <alexyurihallgren@hotmail.com>
2024-11-05 16:05:15 +0100

    changed dialogues, css, added functions to randomly generate health for pokemon.

--- scripts/index.js
+++ scripts/index.js
@@ -1,59 +1,106 @@
 let currentBackground = -1;
-let currentDialogue = 0;
 let playerName = "";
 let playerHealth = 20;
-//playerName = prompt("Hello trainer, what is your name?");
-const redImage = "../images/red.webp";
+const MAX_PLAYER_HEALTH = 20;
+let playerStarterPokemon = "";
+let playerStarterPokemonImage = "";
+let musicActive = false;
+const redImage = "../images/profoak.png";
 const charmanderImage = "../images/charmander.png";
 let prevDialogDiv = null;
-// const backgroundImages = [
-//   redImage,
-//   redImage,
-//   charmanderImage,
-//   "https://img.myshopline.com/image/store/1705286183817/Pokemon-Legends-Arceus-(5)_416x.jpeg?w=1500&h=844&q=80",
-//   "https://img.myshopline.com/image/store/1705286183817/Pokemon-Legends-Arceus-(5)_416x.jpeg?w=1500&h=844&q=80",
-//   "https://img.myshopline.com/image/store/1705286183817/Pokemon-Legends-Arceus-(5)_416x.jpeg?w=1500&h=844&q=80",
-//   "https://img.myshopline.com/image/store/1705286183817/Pokemon-Legends-Arceus-(5)_416x.jpeg?w=1500&h=844&q=80",
-//   "https://img.myshopline.com/image/store/1705286183817/Pokemon-Legends-Arceus-(5)_416x.jpeg?w=1500&h=844&q=80",
-//   "https://img.myshopline.com/image/store/1705286183817/Pokemon-Legends-Arceus-(5)_416x.jpeg?w=1500&h=844&q=80",
-//   "https://img.myshopline.com/image/store/1705286183817/Pokemon-Legends-Arceus-(5)_416x.jpeg?w=1500&h=844&q=80",
-// ];
 
 const dialogueObject = [
-{
-    name: "Red",
-    text: `I have been waiting for you ${playerName}.`,
+  {
+    name: "Prof. Oak",
+    text: `Welcome to the great Pangea, your mother told me you would be arriving soon.`,
     buttonText: "Continue",
-    backgroundImage: redImage
-},
-{
-    name: "Red",
-    text: `There's no time to explain, its dangerous to go alone, take this!`,
+    backgroundImage: redImage,
+  },
+  {
+    name: "Prof. Oak",
+    text: `Let me wish you a warm welcome to the great world of Pokémon! In this world people and Pokémon live side by side.`,
     buttonText: "Continue",
-    backgroundImage: redImage
-},
-{
+    backgroundImage: redImage,
+  },
+  {
+    name: "Prof. Oak",
+    text: `For over a decade we have harnessed the power of Pokémon to accomplish incredible things!`,
+    buttonText: "Continue",
+    backgroundImage: redImage,
+  },
+  {
+    name: "Prof. Oak",
+    text: `And just now it just happens to be your turn to carry on the legacy. I cant wait to see what you will accomplish.`,
+    buttonText: "Continue",
+    backgroundImage: redImage,
+  },
+  {
+    name: "Prof. Oak",
+    text: `But enough about that! I think it's about time for you to pick out your Pokémon, wouldn't you agree?`,
+    buttonText: "Continue",
+    backgroundImage: redImage,
+  },
+  {
+    name: "You",
+    text: `Oh boy this is getting exciting... I wonder which pokémon i'll be able to choose from?`,
+    buttonText: "Continue",
+    action: () => pokemonStarterScene(),
+  },
+  {
     name: "",
-    text: `You recieved a Charmander`,
+    text: `You recieved your very first Starter!`,
     buttonText: "Continue",
-    backgroundImage: charmanderImage
-},
-    
-]    
+    backgroundImage: charmanderImage,
+  },
+];
 
-const playSound = (sound) => {
-  let audio = new Audio(`../mp3/${sound}`) 
-  audio.play();
+const route1HealthGenerator = (level) => {
+  const MAX_HEALTH = Math.min(20, level, 4);
+  return Math.floor(Math.random() * MAX_HEALTH + 1);
 }
 
+const route1 = [
+  {
+    pokemon: "Rattata",
+    level: 3,
+    health: route1HealthGenerator(3),
+    pokemonSprite: ""
+  },
+  {
+    pokemon: "Pidgey",
+    level: 2,
+    health: route1HealthGenerator(2),
+    pokemonSprite: ""
+  }
+]
+
+const startGameMusic = (music, duration) => {
+  let audio = new Audio(`../mp3/${music}`)
+  audio.volume = 0;
+  audio.play();
+
+  let fadeInterval = setInterval(() => {
+    if (audio.volume < 1) {
+        audio.volume = Math.min(audio.volume + 0.05, 1);
+    } else {
+        clearInterval(fadeInterval);
+    }
+}, duration / 20);
+}
+
+const playSound = (sound) => {
+  let audio = new Audio(`../mp3/${sound}`);
+  audio.play();
+};
 
 const switchBackground = () => {
   const startScreen = document.querySelector(".start-screen");
-  
-
+  if (!musicActive) {
+    startGameMusic('pewtercitytheme.mp3', 10000);
+    musicActive = true;
+  }
   startScreen.style.display = "none";
   currentBackground = (currentBackground + 1) % dialogueObject.length;
- 
 
   dialogue(dialogueObject[currentBackground]);
 };
@@ -69,15 +116,21 @@ const dialogue = (dialogueData) => {
   dialogueName.textContent = dialogueData.name;
 
   const dialogueText = document.createElement("p");
+  dialogueText.classList.add("animated-text");
   dialogueText.textContent = dialogueData.text;
 
   const button = document.createElement("button");
+  button.classList.add("animated-button")
   button.textContent = dialogueData.buttonText;
 
   button.addEventListener("click", () => {
     if (button.textContent === "Continue") {
       switchBackground();
-      playSound("hover.mp3")
+      playSound("hover.mp3");
+
+      if (dialogueData.action) {
+        dialogueData.action();
+      }
     }
   });
 
@@ -85,8 +138,32 @@ const dialogue = (dialogueData) => {
   dialogueDiv.appendChild(dialogueText);
   dialogueDiv.appendChild(button);
 
-  if (prevDialogDiv) gamingWindow.removeChild(prevDialogDiv)
+  if (prevDialogDiv) gamingWindow.removeChild(prevDialogDiv);
   gamingWindow.appendChild(dialogueDiv);
   prevDialogDiv = dialogueDiv;
 };
 
+const pokemonStarterScene = () => {
+  const pokemonScene = document.querySelector(".select-pokemon-scene");
+  pokemonScene.style.display = "block";
+  console.log("Jag körde pickpokemon funktionen");
+};
+
+const pickPokemon = (pokemon) => {
+  const pokemonScene = document.querySelector(".select-pokemon-scene");
+
+  if (pokemon === "squirtle") {
+    console.log("Picked squirtle");
+    pokemonScene.style.display = "none";
+    playerStarterPokemon = "squirtle";
+    console.log(playerStarterPokemon);
+  } else if (pokemon === "bulbasaur") {
+    console.log("Picked bulbasaur");
+    pokemonScene.style.display = "none";
+    playerStarterPokemon = "bulbasaur";
+  } else {
+    console.log("Picked charmander");
+    pokemonScene.style.display = "none";
+    playerStarterPokemon = "charmander";
+  }
+};
